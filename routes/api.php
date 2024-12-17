887167253b482472533aa33af6ede8ce1098a170
mafeAS <101601896+mafeAS@users.noreply.github.com>
2024-12-16 22:47:54 -0500

    logout backend

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,8 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/user',function(Request $request){
         return $request->user(); //retorna la informacion del usuario autenticado
     });
+
+    Route::post('/logout',[AuthController::class,'logout']);
 });
 
 
